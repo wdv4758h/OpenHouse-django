9bb6d8c47cc35e146ea2592d6cf4654af1536199
dv <wdv4758h@gmail.com>
2013-12-04 20:30:16 +0800

    add function and url about company
        add company list function
    
        add company url

--- company/urls.py
+++ company/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
+    url(r'', 'company.views.list', name='company'),
 )
--- company/views.py
+++ company/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from company.models import Company
+
+def list(request):
+    companys = Company.objects.all()
+    return render_to_response('company.html', {'companys': companys}, context_instance=RequestContext(request))
