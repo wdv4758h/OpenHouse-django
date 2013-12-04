3b28a97d22af566c5216c5c705b7cff414f06e9f
dv <wdv4758h@gmail.com>
2013-12-04 21:00:48 +0800

    add staff urls setting
        add docs url
    
        add salary url
    
        add salary create url
    
        add staff list url

---
+++ staff/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+
+#tmp
+from django.views.generic import TemplateView
+
+urlpatterns = patterns('',
+    url('docs', TemplateView.as_view(template_name="info.html"), name='docs'),
+    url('salary$', 'staff.views.salary_list', name='salary_list'),
+    url('salary/create', 'staff.views.salary_create', name='salary_create'),
+    url('', 'staff.views.list', name='staff'),
+)
