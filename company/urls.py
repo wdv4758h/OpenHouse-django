3835fb1e2d830e565383ab0cfad0786e1f263bcc
dv <wdv4758h@gmail.com>
2013-12-04 20:46:10 +0800

    add company register and chpass url setting

--- company/urls.py
+++ company/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
+    url(r'register', 'company.views.register', name='company_register'),
+    url(r'chpass', 'company.views.chpass', name='company_chpass'),
     url(r'', 'company.views.list', name='company'),
 )
