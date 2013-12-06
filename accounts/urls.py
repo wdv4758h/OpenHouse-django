5c52f8e3cda2d374618011fc7a5a2abdb09d0379
dv <wdv4758h@gmail.com>
2013-12-06 15:34:07 +0800

    add password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'login.html', 'redirect_field_name' : 'login'}, name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/accounts/login'}, name='logout'),
+    url(r'^password$', 'django.contrib.auth.views.password_change', {'template_name': 'password_change.html', 'post_change_redirect': '/'}, name='password_change'),
 )
