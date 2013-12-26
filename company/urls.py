a1185b4138a9ee62d8f9f2559275e9dfffadb404
dv <wdv4758h@gmail.com>
2013-12-26 17:59:44 +0800

    add company_view function and template

--- company/urls.py
+++ company/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'register', 'company.views.register', name='company_register'),
     url(r'chpass', 'company.views.chpass', name='company_chpass'),
+    url(r'view/(\d+)', 'company.views.view', name='company_view'),
     url(r'', 'company.views.list', name='company'),
 )
--- company/views.py
+++ company/views.py
@@ -6,3 +6,6 @@ from company.models import Company
 def list(request):
     companys = Company.objects.all()
     return render_to_response('company.html', {'companys': companys}, context_instance=RequestContext(request))
+def view(request, num):
+    company = Company.objects.filter(cid=num)[0]
+    return render_to_response('company_view.html', {'company': company}, context_instance=RequestContext(request))
