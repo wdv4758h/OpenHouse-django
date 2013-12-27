377e2e2225d750e1c78f3b2ca4b93a13b0321170
dv <wdv4758h@gmail.com>
2013-12-28 00:23:29 +0800

    update company_view to be more flexible
        change the company_view.html to be more templatable
    
        update view function in company/view.py to fit new template

--- company/views.py
+++ company/views.py
@@ -1,11 +1,29 @@
+# _*_ coding: utf-8 _*_
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from company.models import Company
+
+import glob
 
 def list(request):
     companys = Company.objects.all()
     return render_to_response('company.html', {'companys': companys}, context_instance=RequestContext(request))
 def view(request, num):
     company = Company.objects.filter(cid=num)[0]
-    return render_to_response('company_view.html', {'company': company}, context_instance=RequestContext(request))
+
+    try:
+        img_name = glob.glob1('static/images/company_logo/', company.cid + '-' + company.shortname + '.*')[0].split('/')[-1]
+    except:
+        img_name = ""
+
+    name = company.shortname
+
+    field_names = [ '統編', '公司名稱', '公司簡稱', '類別', '公司電話', '公司地址', '公司網站', '公司簡介', '公司介紹', '人資姓名', '人資電話', '人資傳真', '人資手機', '人資信箱', '更新時間']
+
+    company = [ company.cid, company.name, company.shortname, company.category, company.phone, company.address, company.website, company.brief, company.introduction, company.hr_name, company.hr_phone, company.hr_fax, company.hr_mobile, company.hr_email, company.timestamp ]
+
+    company = zip(field_names, company)
+
+    return render_to_response('company_view.html', {'company': company, 'name': name, 'img': img_name}, context_instance=RequestContext(request))
