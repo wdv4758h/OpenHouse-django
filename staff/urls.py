6eb9e0ab783ca5821f154c3e0c7f636c262fea19
dv <wdv4758h@gmail.com>
2014-02-08 14:54:57 +0800

    add staff view url

--- staff/urls.py
+++ staff/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url('docs', TemplateView.as_view(template_name="info.html"), name='docs'),
     url('salary$', 'staff.views.salary_list', name='salary_list'),
     url('salary/create', 'staff.views.salary_create', name='salary_create'),
+    url(r'view/(.+)', 'staff.views.staff_detail', name='staff_view'),
     url('', 'staff.views.list', name='staff'),
 )
