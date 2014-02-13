b0ccad49faa3420925963092ea30e51057dd4e21
dv <wdv4758h@gmail.com>
2014-02-13 14:56:20 +0800

    add stuvote support

--- event/urls.py
+++ event/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url('dashboard$', 'event.views.dashboard', name='dashboard'),
     url('activity$', 'event.views.event_list', {'list_name': 'activity'}, name='activity'),
     url('sponsor$', 'event.views.event_list', {'list_name': 'sponsor'}, name='activity'),
+    url('stuvote$', 'event.views.event_list', {'list_name': 'vote'}, name='activity'),
 )
