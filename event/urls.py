27583484dd2908329e7c07f254278ce7256e0679
dv <wdv4758h@gmail.com>
2014-02-13 15:36:43 +0800

    add event visit support and fix url

--- event/urls.py
+++ event/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url('dashboard$', 'event.views.dashboard', name='dashboard'),
     url('activity$', 'event.views.event_list', {'list_name': 'activity'}, name='activity'),
-    url('sponsor$', 'event.views.event_list', {'list_name': 'sponsor'}, name='activity'),
-    url('stuvote$', 'event.views.event_list', {'list_name': 'vote'}, name='activity'),
-    url('teach$', 'event.views.event_list', {'list_name': 'teach'}, name='activity'),
+    url('sponsor$', 'event.views.event_list', {'list_name': 'sponsor'}, name='sponsor'),
+    url('stuvote$', 'event.views.event_list', {'list_name': 'vote'}, name='vote'),
+    url('teach$', 'event.views.event_list', {'list_name': 'teach'}, name='teach'),
+    url('visit$', 'event.views.event_list', {'list_name': 'visit'}, name='visit'),
 )
