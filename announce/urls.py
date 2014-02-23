b66d055d5f8c80411dc14dfc55035c1fc661f792
dv <wdv4758h@gmail.com>
2014-02-23 14:58:36 +0800

    add announce detail view

--- announce/urls.py
+++ announce/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
+from announce import views
 
 urlpatterns = patterns('',
+    url(r'view/(?P<pk>\d+)', views.News.as_view(), name='announce_view'),
     url('', 'announce.views.news', name='announce'),
 )
--- announce/views.py
+++ announce/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 from announce.models import Announce
 
 @login_required(redirect_field_name='index')
@@ -11,3 +12,11 @@ def news(request):
     categorys = ['#', '標題', '發佈時間', '更新時間', '檢視']
     header = '最新訊息'
     return render_to_response('announce.html', {'header': header, 'items': news, 'categorys': categorys}, context_instance=RequestContext(request))
+
+class News(ListView):
+    model = Announce
+    template_name = 'announce_view.html'
+
+    def get(self, request, *args, **kwargs):
+        self.queryset = Announce.objects.filter(id=self.kwargs['pk'])[0]
+        return super(News, self).get(request, *args, **kwargs)
