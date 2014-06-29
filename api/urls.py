7bda0aad6dd37d715c1ea2ad18b23de5b76ee9f6
dv <wdv4758h@gmail.com>
2014-06-29 15:32:43 +0800

    update news api

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,12 @@ from rest_framework import routers
 
 router = routers.DefaultRouter(trailing_slash=True)
 
+inner_urls = patterns('',
+    url('^news/', views.NewsList.as_view(), name='news-list'),
+    url('', include(router.urls)),
+)
+
 urlpatterns = patterns('',
-    url('(?P<year>\d{4})/news', views.NewsViewSet.as_view()),
-    url(r'^', include(router.urls)),
+    #url(r'^(?P<year>\d{4})/', include(inner_urls)),
+    url(r'^\d{4}/', include(inner_urls)),
 )
--- api/views.py
+++ api/views.py
@@ -2,14 +2,16 @@ from rest_framework.generics import ListAPIView
 from api.models import NewsSerializer
 from ohcms.models import News, NewsIndex
 
-class NewsViewSet(ListAPIView):
+class NewsList(ListAPIView):
     """
-    API endpoint that allows users to be viewed or edited.
+    API for News
     """
 
     serializer_class = NewsSerializer
 
     def get_queryset(self):
         # assume root page url is 'home'
-        parent = NewsIndex.objects.get(url_path='/home/{}/news/'.format(self.kwargs['year']))
+        year = self.request.get_full_path().split('/')[2]
+        #year = self.kwargs['year']
+        parent = NewsIndex.objects.get(url_path='/home/{}/news/'.format(year))
         return News.objects.live().descendant_of(parent)
