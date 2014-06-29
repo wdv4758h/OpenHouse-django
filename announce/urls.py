1117c830989d54d4637ce1b8dd83377e9d5d0821
dv <wdv4758h@gmail.com>
2014-06-29 20:19:13 +0800

    remove old news

--- announce/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from announce import views
-
-urlpatterns = patterns('',
-    url(r'view/(?P<pk>\d+)', views.News.as_view(), name='announce_view'),
-    url('create', views.AnnounceCreate.as_view(), name='announce_create'),
-    url('', 'announce.views.news', name='announce'),
-)
