5f31217a16f2a67a0fc2eddc7dc9b0ce719df1bf
dv <wdv4758h@gmail.com>
2014-07-01 02:20:36 +0800

    add staff view for wagtail

--- staff/views.py
+++ staff/views.py
@@ -1,76 +1,114 @@
-# _*_ coding: utf8 _*_
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
+from django.db.models import Q
+from django.utils.translation import ugettext as _
+from django.views.decorators.vary import vary_on_headers
 
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.http import HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import UpdateView
-from django.core.urlresolvers import reverse_lazy
-from staff.models import Staff, Resume, Salary, SalaryForm, StaffForm
+from wagtail.wagtailadmin.forms import SearchForm
+from staff.forms import StaffCreationForm as UserCreationForm, StaffEditForm as UserEditForm
+from wagtail.wagtailcore.compat import AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME
 
-@login_required(redirect_field_name='index')
-def list(request):
-    staffs = Staff.objects.all()
-    categorys = ['學號', '姓名', '職稱', '手機號碼', 'E-mail', 'OH BBS 帳號', '動作']
-    header = '工作人員名單'
-    return render_to_response('staff.html', {'header': header, 'items': staffs, 'categorys': categorys}, context_instance=RequestContext(request))
+from staff.models import Staff
 
-@login_required(redirect_field_name='index')
-def salary_create(request):
-    if request.method == 'POST':
-        form = SalaryForm(request.POST)
+User = Staff
+
+# Typically we would check the permission 'auth.change_user' for user
+# management actions, but this may vary according to the AUTH_USER_MODEL
+# setting
+change_user_perm = "{0}.change_{1}".format(AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME.lower())
+
+
+@permission_required(change_user_perm)
+@vary_on_headers('X-Requested-With')
+def index(request):
+    q = None
+    p = request.GET.get("p", 1)
+    is_searching = False
+
+    if 'q' in request.GET:
+        form = SearchForm(request.GET, placeholder=_("Search users"))
         if form.is_valid():
-            new_salary = form.save()
-            return HttpResponseRedirect("/staffs/salary")
+            q = form.cleaned_data['q']
+
+            is_searching = True
+            users = User.objects.filter(Q(studentid__icontains=q) | Q(name__icontains=q) | Q(email__icontains=q))
     else:
-        form = SalaryForm()
+        form = SearchForm(placeholder=_("Search users"))
 
-    #add 'form-control' class and placeholder
-    for field in form.fields.keys():
-        field_obj = form.fields[field]
+    if not is_searching:
+        users = User.objects
 
-        field_obj.widget.attrs = {
-                'class' : 'form-control',
-                'placeholder' : field_obj.help_text}
-        field_obj.help_text = ''
+    users = users.order_by('studentid')
 
-    return render_to_response('salary_create.html', {'form': form}, context_instance=RequestContext(request))
+    if 'ordering' in request.GET:
+        ordering = request.GET['ordering']
 
-@login_required(redirect_field_name='index')
-def salary_list(request):
-    salarys = Salary.objects.all().order_by('id').reverse()
-    for i in salarys:
-        try:
-            #person = Staff.objects.filter(studentid=i.staff_id)[0]
-            person = Staff.objects.filter(id=i.staff_id)[0]
-            (i.studentid, i.name, i.role) = (person.studentid, person.name, person.role)
-        except:
-            pass
+        if ordering in ['name', 'studentid']:
+            if ordering != 'studentid':
+                users = users.order_by(ordering)
+    else:
+        ordering = 'studentid'
 
-    return render_to_response('salary_list.html', {'salarys': salarys}, context_instance=RequestContext(request))
+    paginator = Paginator(users, 20)
 
-@login_required(redirect_field_name='index')
-def staff_detail(request, num):
-    staff = Staff.objects.filter(studentid=num)[0]
-    preheader = '檢視工作人員 - '
-    header = staff.name
-    fields = ['學號', '姓名', '性別', '出生年月日', '職稱', '手機號碼', 'E-mail', 'FB個人首頁連結', 'BS2帳號', 'OH BBS帳號', '郵局帳號', '審核通過', '更新時間']
+    try:
+        users = paginator.page(p)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
 
-    staff = (staff.studentid, staff.name, staff.gender, staff.birthday, staff.role, staff.mobile, staff.email, staff.fb_url, staff.bs2id, staff.ohbbsid, staff.postacct, staff.verify, staff.timestamp)
+    if request.is_ajax():
+        return render(request, "staff/results.html", {
+            'users': users,
+            'is_searching': is_searching,
+            'query_string': q,
+            'ordering': ordering,
+        })
+    else:
+        return render(request, "staff/index.html", {
+            'search_form': form,
+            'users': users,
+            'is_searching': is_searching,
+            'ordering': ordering,
+            'query_string': q,
+        })
 
-    staff = zip(fields, staff)
+@permission_required(change_user_perm)
+def create(request):
+    if request.POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, _("User '{0}' created.").format(user))
+            return redirect('staff_index')
+        else:
+            messages.error(request, _("The user could not be created due to errors.") )
+    else:
+        form = UserCreationForm()
 
-    return render_to_response('detailview.html', {'preheader': preheader, 'header': header, 'data': staff}, context_instance=RequestContext(request))
+    return render(request, 'staff/create.html', {
+        'form': form,
+    })
 
-class StaffUpdate(UpdateView):
-    model = Staff
-    template_name = 'staff_update.html'
-    success_url = reverse_lazy('staff')
-    form_class = StaffForm
 
-    def get_object(self, queryset=None):
-        obj = Staff.objects.get(studentid=self.kwargs['pk'])
-        return obj
+@permission_required(change_user_perm)
+def edit(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if request.POST:
+        form = UserEditForm(request.POST, instance=user)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, _("User '{0}' updated.").format(user))
+            return redirect('staff_index')
+        else:
+            messages.error(request, _("The user could not be saved due to errors."))
+    else:
+        form = UserEditForm(instance=user)
 
-    def dispatch(self, request, *args, **kwargs):
-       return super(StaffUpdate, self).dispatch(request, *args, **kwargs)
+    return render(request, 'staff/edit.html', {
+        'user': user,
+        'form': form,
+    })
