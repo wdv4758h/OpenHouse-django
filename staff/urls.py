9fb7ae2d6a098c77fb767e367c9645d8516895d1
dv <wdv4758h@gmail.com>
2014-07-01 02:21:19 +0800

    update stff urls

--- staff/urls.py
+++ staff/urls.py
@@ -1,14 +1,8 @@
-from django.conf.urls import patterns, url
-from . import views
+from django.conf.urls import url
+from staff import views
 
-#tmp
-from django.views.generic import TemplateView
-
-urlpatterns = patterns('',
-    url('docs', TemplateView.as_view(template_name="info.html"), name='docs'),
-    url('salary$', 'staff.views.salary_list', name='salary_list'),
-    url('salary/create', 'staff.views.salary_create', name='salary_create'),
-    url(r'view/(.+)', 'staff.views.staff_detail', name='staff_view'),
-    url(r'update/(?P<pk>\d+)', views.StaffUpdate.as_view(), name='staff_update'),
-    url('', 'staff.views.list', name='staff'),
-)
+urlpatterns = [
+    url(r'^$', views.index, name='staff_index'),
+    url(r'^new/$', views.create, name='staff_create'),
+    url(r'^(\d+)/$', views.edit, name='staff_edit'),
+]
