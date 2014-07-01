0036b59d85501c81d787498e6d27bc24b38484b7
dv <wdv4758h@gmail.com>
2014-07-01 20:41:41 +0800

    change company/views.py a lots

--- company/views.py
+++ company/views.py
@@ -1,69 +1,124 @@
-# _*_ coding: utf-8 _*_
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
+from django.db.models import Q
+from django.utils.translation import ugettext as _
+from django.views.decorators.vary import vary_on_headers
 
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import UpdateView
-from django.core.urlresolvers import reverse_lazy
-from company.models import Company, CompanyForm
+from wagtail.wagtailadmin.forms import SearchForm
+from company.forms import CompanyCreationForm as UserCreationForm, CompanyEditForm as UserEditForm
+from wagtail.wagtailcore.compat import AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME
 
-import glob
+from company.models import Company
 
-@login_required(redirect_field_name='index')
-def list(request):
-    companys = Company.objects.all()
-    return render_to_response('company.html', {'companys': companys}, context_instance=RequestContext(request))
+User = Company
 
-@login_required(redirect_field_name='index')
-def register(request):
-    if request.method == 'POST':
-        form = CompanyForm(request.POST)
+# Typically we would check the permission 'auth.change_user' for user
+# management actions, but this may vary according to the AUTH_USER_MODEL
+# setting
+change_user_perm = "{0}.change_{1}".format(AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME.lower())
+
+@permission_required(change_user_perm)
+@vary_on_headers('X-Requested-With')
+def index(request):
+    q = None
+    p = request.GET.get("p", 1)
+    is_searching = False
+
+    if 'q' in request.GET:
+        form = SearchForm(request.GET, placeholder=_("Search users"))
         if form.is_valid():
-            new_company = form.save()
-            #not yet prompt success or not
-            return HttpResponseRedirect("/company/")
+            q = form.cleaned_data['q']
+
+            is_searching = True
+            users = User.objects.filter(
+                Q(cid__icontains=q) |
+                Q(shortname__icontains=q) |
+                Q(name__icontains=q) |
+                Q(category__icontains=q) |
+                Q(hr_email__icontains=q) |
+                Q(hr_phone__icontains=q) |
+                Q(hr_mobile__icontains=q) |
+                Q(hr_email__icontains=q)
+            )
     else:
-        form = CompanyForm()
+        form = SearchForm(placeholder=_("Search users"))
 
-    #add 'form-control' class and placeholder
-    for field in form.fields.keys():
-        field_obj = form.fields[field]
+    if not is_searching:
+        users = User.objects
 
-        field_obj.widget.attrs = {
-                'class' : 'form-control',
-                'placeholder' : field_obj.help_text}
-        field_obj.help_text = ''
+    users = users.order_by('cid')
 
-    return render_to_response('company_create.html', {'form': form}, context_instance=RequestContext(request))
+    if 'ordering' in request.GET:
+        ordering = request.GET['ordering']
 
-@login_required(redirect_field_name='index')
-def view(request, num):
-    company = Company.objects.filter(cid=num)[0]
+        if ordering in ['name', 'cid']:
+            if ordering != 'cid':
+                users = users.order_by(ordering)
+    else:
+        ordering = 'cid'
+
+    paginator = Paginator(users, 20)
 
     try:
-        img_name = glob.glob1('static/images/company_logo/', company.cid + '-' + company.shortname + '.*')[0].split('/')[-1]
-    except:
-        img_name = ""
+        users = paginator.page(p)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
 
-    name = company.shortname
+    if request.is_ajax():
+        return render(request, "company/results.html", {
+            'users': users,
+            'is_searching': is_searching,
+            'query_string': q,
+            'ordering': ordering,
+        })
+    else:
+        return render(request, "company/index.html", {
+            'search_form': form,
+            'users': users,
+            'is_searching': is_searching,
+            'ordering': ordering,
+            'query_string': q,
+        })
 
-    field_names = [ '統編', '公司名稱', '公司簡稱', '類別', '公司電話', '公司地址', '公司網站', '公司簡介', '公司介紹', '人資姓名', '人資電話', '人資傳真', '人資手機', '人資信箱', '更新時間']
+@permission_required(change_user_perm)
+def create(request):
+    if request.POST:
+        # Company has logo which is ImageField,
+        # need pass in request.FILES
+        form = UserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, _("User '{0}' created.").format(user))
+            return redirect('company_index')
+        else:
+            messages.error(request, _("The user could not be created due to errors.") )
+    else:
+        form = UserCreationForm()
 
-    company = [ company.cid, company.name, company.shortname, company.category, company.phone, company.address, company.website, company.brief, company.introduction, company.hr_name, company.hr_phone, company.hr_fax, company.hr_mobile, company.hr_email, company.timestamp ]
+    return render(request, 'company/create.html', {
+        'form': form,
+    })
 
-    company = zip(field_names, company)
 
-    return render_to_response('company_view.html', {'company': company, 'name': name, 'img': img_name}, context_instance=RequestContext(request))
+@permission_required(change_user_perm)
+def edit(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if request.POST:
+        form = UserEditForm(request.POST, instance=user)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, _("User '{0}' updated.").format(user))
+            return redirect('company_index')
+        else:
+            messages.error(request, _("The user could not be saved due to errors."))
+    else:
+        form = UserEditForm(instance=user)
 
-class CompanyUpdate(UpdateView):
-    model = Company
-    template_name = 'company_update.html'
-    success_url = reverse_lazy('company')
-    form_class = CompanyForm
-
-    def get_object(self, queryset=None):
-        obj = Company.objects.get(cid=self.kwargs['pk'])
-        return obj
-
-    def dispatch(self, request, *args, **kwargs):
-       return super(CompanyUpdate, self).dispatch(request, *args, **kwargs)
+    return render(request, 'company/edit.html', {
+        'user': user,
+        'form': form,
+    })
