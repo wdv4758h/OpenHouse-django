7dcb00177d7d7cc482ef8420269ddb222c55d64a
dv <wdv4758h@gmail.com>
2014-07-01 16:22:09 +0800

    remove PermissionsMixin import from Staff

--- staff/models.py
+++ staff/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager, Group
+from django.contrib.auth.models import AbstractBaseUser, UserManager, Group
 
 class Staff(AbstractBaseUser):
     """
