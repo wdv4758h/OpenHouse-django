afa83f0c96afb5d928683a92f13bb96851a997ec
dv <wdv4758h@gmail.com>
2014-07-01 20:42:52 +0800

    update company urls.py

--- company/urls.py
+++ company/urls.py
@@ -1,11 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from company import views
 
-from . import views
-
-urlpatterns = patterns('',
-    url(r'register', 'company.views.register', name='company_register'),
-    url(r'chpass', 'company.views.chpass', name='company_chpass'),
-    url(r'view/(\d+)', 'company.views.view', name='company_view'),
-    url(r'update/(?P<pk>\d+)', views.CompanyUpdate.as_view(), name='company_update'),
-    url(r'', 'company.views.list', name='company'),
-)
+urlpatterns = [
+    url(r'^$', views.index, name='company_index'),
+    url(r'^new/$', views.create, name='company_create'),
+    url(r'^(\d+)/$', views.edit, name='company_edit'),
+]
