40f7d2e9496af6c20ab0e912d8996b9457698c37
dv <wdv4758h@gmail.com>
2014-07-02 23:48:03 +0800

    remove redundant code for login/logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    #url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'login.html', 'redirect_field_name' : 'login'}, name='login'),
-    url(r'^login$', 'accounts.views.OHlogin', name='login'),
-    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/accounts/login'}, name='logout'),
+    url(r'^login[/]$', 'django.contrib.auth.views.login', {'template_name': 'login.html', 'redirect_field_name' : 'index'}, name='login'),
+    url(r'^logout[/]$', 'django.contrib.auth.views.logout', {'next_page': '/2015/'}, name='logout'),
     url(r'^password$', 'django.contrib.auth.views.password_change', {'template_name': 'password_change.html', 'post_change_redirect': '/'}, name='password_change'),
 )
