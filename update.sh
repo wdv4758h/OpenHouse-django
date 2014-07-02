c31aac6d4debb4d6a7274288bdd1497b0903db10
dv <wdv4758h@gmail.com>
2014-07-03 02:39:09 +0800

    add git pull check to update script

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 source env/bin/activate
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U
-cd wagtail && git pull && python2 setup.py install
+
+cd wagtail
+status=$(git pull)
+if [ "$status" != "Already up-to-date." ]; then
+    python2 setup.py install
+fi
 cd ..
+
 echo "yes" | python2 manage.py collectstatic
