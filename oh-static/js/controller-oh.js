31f96e1af50dcf52e7fd98bb53114eebbd4e6da2
dv <wdv4758h@gmail.com>
2014-07-03 21:41:07 +0800

    remove unuse javascript libs and update using libs

--- oh-static/js/controller-oh.js
+++
@@ -1,4 +0,0 @@
-var app = angular.module('OpenHouse', ['mgcrea.ngStrap'], function($interpolateProvider) {
-    $interpolateProvider.startSymbol('[[');
-    $interpolateProvider.endSymbol(']]');
-});
