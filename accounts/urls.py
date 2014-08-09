be1c7dddf1d1fcf37986eded9e916f4db5a71f9b
dv <wdv4758h@gmail.com>
2014-08-09 20:56:53 +0800

    change accounts logout redirect path to read from settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^login[/]$', 'django.contrib.auth.views.login', {'template_name': 'login.html', 'redirect_field_name' : 'index'}, name='login'),
-    url(r'^logout[/]$', 'django.contrib.auth.views.logout', {'next_page': '/2015/'}, name='logout'),
+    url(r'^logout[/]$', 'django.contrib.auth.views.logout', {'next_page': settings.YEAR + '/'}, name='logout'),
     url(r'^password$', 'django.contrib.auth.views.password_change', {'template_name': 'password_change.html', 'post_change_redirect': '/'}, name='password_change'),
 )
