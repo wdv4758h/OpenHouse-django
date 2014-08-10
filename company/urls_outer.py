a9901533959f2e89bbf206f01a36d1701b66c4f8
dv <wdv4758h@gmail.com>
2014-08-10 16:53:17 +0800

    add info for company itself

--- company/urls_outer.py
+++ company/urls_outer.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 from company.views_outer import hrdb_view
 
 urlpatterns = [
     url(r'^hrdb/', hrdb_view, name='company_hrdb'),
+    url(r'^view/', login_required(TemplateView.as_view(template_name='company/view.html')), name='company_view'),
 ]
