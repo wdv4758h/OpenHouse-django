21c1659db796841dd9f445f4cfd9f40a96e8e5ac
dv <wdv4758h@gmail.com>
2014-08-10 15:22:33 +0800

    add script for prepend 'http://' to exist url

---
+++ company/url_update.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.db.models import Q
+from company.models import Company
+
+for i in Company.objects.filter(~Q(website__startswith='http')):
+    i.website = 'http://' + i.website
+    i.save()
